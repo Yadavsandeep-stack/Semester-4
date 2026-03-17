3d36d6ad583382769811643b1a68f1fd478d508f
Yadavsandeep-stack <sy518598@gmail.com>
2026-03-17 21:49:03 +0530

    first commit

--- Semester 4/main.cpp
+++ Semester 4/main.cpp
@@ -1,14 +1,5 @@
-//
-//  main.cpp
-//  Semester 4
-//
-//  Created by Sandeep Yadav on 17/03/26.
-//
-
-#include <iostream>
-
-int main(int argc, const char * argv[]) {
-    
-    std::cout << "Hello, World!\n";
-    return EXIT_SUCCESS;
+#include<iostream>
+using namespace std;
+int main(){
+    cout<<"Hello World !!!";
 }
